e362483cb545471487b17658ba7e0f78edfb6f50
himanshu2408 <singh.himanshu2408@gmail.com>
2017-06-04 21:56:57 +0530

    edit url

--- src/app/services/static.service.ts
+++ src/app/services/static.service.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class StaticService{
 
-  private Url = 'http://192.168.1.5:3000/api/';
+  private Url = 'http://192.168.1.4:3000/api/';
   constructor() { }
   getUrl (){
     return this.Url;
