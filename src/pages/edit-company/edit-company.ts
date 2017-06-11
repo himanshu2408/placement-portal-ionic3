c65a00241e0b21d173bc4b124f11a1ea6badd910
himanshu2408 <singh.himanshu2408@gmail.com>
2017-06-11 09:26:08 +0530

    edit company form validation

--- src/pages/edit-company/edit-company.ts
+++ src/pages/edit-company/edit-company.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CompaniesService } from '../../app/services/companies.service';
 import { AlertController } from 'ionic-angular';
 import { RegisteredStudentsPage } from '../registered-students/registered-students';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -12,15 +13,21 @@ import { RegisteredStudentsPage } from '../registered-students/registered-studen
 export class EditCompanyPage {
 
   private company: any;
-
+  public editCompanyForm: FormGroup;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private companiesService: CompaniesService,
-    private alertCtrl: AlertController
+    private alertCtrl: AlertController,
+    public formBuilder: FormBuilder
   ) {
     this.company = navParams.get('company');
-    console.log(this.company);
+    this.editCompanyForm = formBuilder.group({
+      name: [this.company.name, Validators.compose([Validators.required])],
+      profile: [this.company.profile, Validators.compose([Validators.required])],
+      ctc: [this.company.ctc, Validators.compose([Validators.required])],
+      address: [this.company.address, Validators.compose([Validators.required])]
+    });
   }
 
   ionViewDidLoad() {
@@ -28,6 +35,11 @@ export class EditCompanyPage {
   }
 
   save() {
+    this.company.name = this.editCompanyForm.value.name;
+    this.company.profie = this.editCompanyForm.value.profile;
+    this.company.ctc = this.editCompanyForm.value.ctc;
+    this.company.address = this.editCompanyForm.value.address;
+
     this.companiesService.update(this.company)
       .subscribe(response => {
         this.presentAlert();
